4e6eca517ff607081cf621aa6a74111003651d81
kwr79 <kaspar.roos@aspireccs.com>
2025-01-01 13:00:58 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
   output: 'export',
-  distDir: 'dist',
   images: {
     unoptimized: true
   }
