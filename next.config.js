4b7f64dc391c14659d5cd38ec2e00fdb4e742d89
kwr79 <kaspar.roos@aspireccs.com>
2025-01-01 13:51:50 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  output: 'export',
+  trailingSlash: true,
   images: {
     unoptimized: true
-  }
+  },
+  basePath: '',
+  assetPrefix: ''
 }
 
 module.exports = nextConfig
