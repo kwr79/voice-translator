7ad88f3da27ccd1c801ab16d1633d90d7686b543
kwr79 <kaspar.roos@aspireccs.com>
2025-01-01 12:27:56 +0100

    Delete app/page.tsx.ts

--- app/page.tsx.ts
+++
@@ -1,9 +0,0 @@
-import VoiceTranslator from './components/VoiceTranslator'
-
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <VoiceTranslator />
-    </main>
-  )
-}
